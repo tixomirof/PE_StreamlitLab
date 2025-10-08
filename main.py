539d708212ad77625b9827b7229cf8b0600b4487
Vaik Helby <valientin.rudin@mail.ru>
2025-10-08 22:18:58 +0500

    fix

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ if uploaded_file:
     }
     for key in label_counts.keys():
         result["Эмоции"].append(key)
-        result["Эмоции"].append(label_counts[key])
+        result["Количество"].append(label_counts[key])
     result
     bar_colors = [colors.get(category, '#888888') for category in label_counts.keys()]
     st.bar_chart(result, x="Эмоции", y= "Количество", color=bar_colors, horizontal=False)    
